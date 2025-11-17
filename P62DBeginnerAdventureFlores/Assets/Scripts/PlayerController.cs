bc2f4afc59af65672cdcc24c6946ecc165985848
sammythegamerx5-code <sammythegamerx5@gmail.com>
2025-11-17 09:15:03 -0800

    Moving
    I did a good dent in decoration of the game  and moving the character with keyboard buttons

--- P62DBeginnerAdventureFlores/Assets/Scripts/PlayerController.cs
+++ P62DBeginnerAdventureFlores/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
+    public InputAction LeftAction;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+            Debug.Log(horizontal);
+
+
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
+
+
+            Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
