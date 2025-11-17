06cf912b533c850d584a720fd2f62ca948e91198
sammythegamerx5-code <sammythegamerx5@gmail.com>
2025-11-17 09:58:56 -0800

    Fixed move scripts
    Fixed the controller script so that I can correctly move character.

--- P62DBeginnerAdventureFlores/Assets/Scripts/PlayerController.cs
+++ P62DBeginnerAdventureFlores/Assets/Scripts/PlayerController.cs
@@ -1,48 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
 public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
-    public InputAction LeftAction;
+    //public InputAction LeftAction;
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
+        MoveAction.Enable();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-            Debug.Log(horizontal);
-
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-
-
-            Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+                              
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
